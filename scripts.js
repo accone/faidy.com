0d69bcf508ae88cd8254c20d8f2cb1c5090e2d53
TShaBa <avira222217@gmail.com>
2020-03-06 00:08:52 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
-$( document ).ready(function() {
-    $('.vote').on ('click', function(){
-        $(this).addClass('active');
-        var parent = $(this).parent().parent().parent().parent();
-        var commentCountElement = parent.find('.commentscount');
-        var ratingValueElement = parent.find('.ratingvalue');
-        var votedValue = parseInt($(this).attr('data-score'));
-        parent.addClass('voted');
-        var commentCount = parseInt(commentCountElement.text());
-        commentCountElement.text(commentCount + 1);
-        var rating = parseFloat(ratingValueElement.text());
-        rating = (commentCount * rating + votedValue)/(commentCount + 1);
-        ratingValueElement.text(rating.toFixed(2));
-        console.log('Пользователь поставил оценку - ' + votedValue);
+let click = [];
+    $( document ).ready(function() {
+        $('.vote').on ('click', function(){
+            $(this).addClass('active');
+            var parent = $(this).parent().parent().parent().parent();
+            var commentCountElement = parent.find('.commentscount');
+            var ratingValueElement = parent.find('.ratingvalue');
+            var votedValue = parseInt($(this).attr('data-score'));
+            parent.addClass('voted');
+            var commentCount = parseInt(commentCountElement.text());
+            commentCountElement.text(commentCount + 1);
+            var rating = parseFloat(ratingValueElement.text());
+            rating = (commentCount * rating + votedValue)/(commentCount + 1);
+            ratingValueElement.text(rating.toFixed(2));
+            console.log('Пользователь поставил оценку - ' + votedValue);
+            localStorage.setItem('click', JSON.stringify(click));
+            if (localStorage.getItem('click')) click = JSON.parse(localStorage.getItem('click'));
     })
 })
