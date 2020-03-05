55047e5e45fb99b228a6c849e061d03fce818452
TShaBa <avira222217@gmail.com>
2020-03-06 00:04:57 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+    $('.vote').on ('click', function(){
+        $(this).addClass('active');
+        var parent = $(this).parent().parent().parent().parent();
+        var commentCountElement = parent.find('.commentscount');
+        var ratingValueElement = parent.find('.ratingvalue');
+        var votedValue = parseInt($(this).attr('data-score'));
+        parent.addClass('voted');
+        var commentCount = parseInt(commentCountElement.text());
+        commentCountElement.text(commentCount + 1);
+        var rating = parseFloat(ratingValueElement.text());
+        rating = (commentCount * rating + votedValue)/(commentCount + 1);
+        ratingValueElement.text(rating.toFixed(2));
+        console.log('Пользователь поставил оценку - ' + votedValue);
+    })
+})
